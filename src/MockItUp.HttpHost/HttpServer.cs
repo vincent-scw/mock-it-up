8a3fb6910452f51d3b268f2137a62272eacaf367
Vincent Shen <Vincent.Shen@chrobinson.com>
2021-01-21 14:45:55 +0800

    add HttpServer

---
+++ src/MockItUp.HttpHost/HttpServer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MockItUp.HttpHost
+{
+    public class HttpServer
+    {
+        public async Task StartAsync()
+        {
+
+        }
+    }
+}
