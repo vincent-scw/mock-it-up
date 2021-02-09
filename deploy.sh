84e4f5860d44666257bcf9faaefbfa3c12a472d5
Vincent Shen <Vincent.Shen@chrobinson.com>
2021-02-09 14:51:35 +0800

    try

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-if [ "$TRAVIS_PULL_REQUEST" = "false" -a "$TRAVIS_BRANCH" = "main" ]; then
-  docker build -t edentidus/mockitup:v$VERSION_NUMBER -f ./src/MockItUp.Console/Dockerfile .
-  echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-  docker push edentidus/mockitup:v$VERSION_NUMBER
-  dotnet nuget push src/MockItUp.Client\bin\Release\MockItUp.Client.$VERSION_NUMBER.nupkg --api-key $NUGET_KEY --source https://api/nuget.org/v3/index.json
-fi
+docker build -t edentidus/mockitup:v$VERSION_NUMBER -f ./src/MockItUp.Console/Dockerfile .
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker push edentidus/mockitup:v$VERSION_NUMBER
+dotnet nuget push src/MockItUp.Client\bin\Release\MockItUp.Client.$VERSION_NUMBER.nupkg --api-key $NUGET_KEY --source https://api/nuget.org/v3/index.json
