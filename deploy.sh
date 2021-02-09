89b38759ba67c47e06e614d2631b762a4a85133a
Vincent Shen <Vincent.Shen@chrobinson.com>
2021-02-09 15:27:19 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker build -f ./src/MockItUp.Console/Dockerfile ./src
+docker build -t edentidus/mockitup:v$VERSION_NUMBER -f ./src/MockItUp.Console/Dockerfile ./src
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push edentidus/mockitup:v$VERSION_NUMBER
-dotnet nuget push ./src/MockItUp.Client/bin/Release/MockItUp.Client.$VERSION_NUMBER.nupkg --api-key $NUGET_KEY --source https://api/nuget.org/v3/index.json
+dotnet nuget push ./src/MockItUp.Client/bin/Release/MockItUp.Client.$VERSION_NUMBER.nupkg --api-key $NUGET_KEY --source https://api.nuget.org/v3/index.json
