c19e887b241084c46856e228cd71acb82d728cf5
Vincent Shen <SHENVIN@chrobinson.com>
2022-01-27 13:31:32 +0800

    Create StubItemTest.cs

---
+++ test/MockItUp.UnitTest/Core/StubItemTest.cs
@@ -0,0 +1,19 @@
+﻿using MockItUp.Core.Models;
+using System;
+using Xunit;
+
+namespace MockItUp.UnitTest.Core
+{
+    public class StubItemTest
+    {
+        [Theory]
+        [InlineData("/api/orders", "https://test.com/api/orders")]
+        //[InlineData("/api/orders", "https://test.com/api/orders/")]
+        public void Url_ShouldMatch(string urlTemplate, string urlCandidate)
+        {
+            var stub = new StubItem() { Request = new RequestModel { Method = "get", Path = urlTemplate } };
+            var matched = stub.Match("get", new Uri(urlCandidate));
+            Assert.NotNull(matched);
+        }
+    }
+}
