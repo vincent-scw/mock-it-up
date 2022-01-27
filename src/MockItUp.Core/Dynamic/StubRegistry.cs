e3c965be48c6e0244a94e898e45659c0a4ea8ecb
Vincent Shen <SHENVIN@chrobinson.com>
2022-01-27 10:45:00 +0800

    fix multi thread issue

--- src/MockItUp.Core/Dynamic/StubRegistry.cs
+++ src/MockItUp.Core/Dynamic/StubRegistry.cs
@@ -8,10 +8,12 @@ namespace MockItUp.Core.Dynamic
 {
     public class StubRegistry : IStubRegistry
     {
-        private readonly ConcurrentDictionary<string, IList<StubItem>> _stubDict;
+        private readonly Dictionary<string, IList<StubItem>> _stubDict;
+        private readonly object _syncObj;
         public StubRegistry()
         {
-            _stubDict = new ConcurrentDictionary<string, IList<StubItem>>();
+            _stubDict = new Dictionary<string, IList<StubItem>>();
+            _syncObj = new object();
         }
 
         public IReadOnlyDictionary<string, IList<StubItem>> Stubs => _stubDict;
@@ -25,26 +27,32 @@ namespace MockItUp.Core.Dynamic
             var id = IdGen.Generate();
             stub.SetID(id);
 
-            IList<StubItem> items;
-            if (_stubDict.ContainsKey(key))
-                items = _stubDict[key];
-            else
-                items = new List<StubItem>();
+            lock (_syncObj)
+            {
+                IList<StubItem> items;
+                if (_stubDict.ContainsKey(key))
+                    items = _stubDict[key];
+                else
+                    items = new List<StubItem>();
 
-            // Insert the new stub to be the first one
-            items.Insert(0, stub);
-            _stubDict[key] = items;
+                // Insert the new stub to be the first one
+                items.Insert(0, stub);
+                _stubDict[key] = items;
+            }
 
             return id;
         }
 
         public void Remove(IList<string> ids)
         {
-            var keys = new string[_stubDict.Keys.Count];
-            _stubDict.Keys.CopyTo(keys, 0);
-            foreach (var key in keys)
+            lock (_syncObj)
             {
-                _stubDict[key] = _stubDict[key].Where(x => !ids.Contains(x.ID.ToString())).ToList();
+                var keys = new string[_stubDict.Keys.Count];
+                _stubDict.Keys.CopyTo(keys, 0);
+                foreach (var key in keys)
+                {
+                    _stubDict[key] = _stubDict[key].Where(x => !ids.Contains(x.ID.ToString())).ToList();
+                }
             }
         }
     }
